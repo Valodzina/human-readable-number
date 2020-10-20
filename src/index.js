9b6b8a7398bb36be7c2712c7b775da2754e4f016
Ulyana Valodzina <67861703+Valodzina@users.noreply.github.com>
2020-10-20 20:13:13 +0300

    feat do work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function toReadable (number) {
   
+let mas = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+let m= ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred"];
+if (number<=19)
+return mas[number];
+let fir;
+let sec;
+let thir;
+fir = number%10;
+sec= Math.trunc(number/10);
+if (number<100)
+{
+    if(fir===0)
+    return m[sec-2];
+    return m[sec-2]+" "+mas[fir];
+}
+sec = sec%10;
+thir= Math.trunc(number/100);
+if(fir===0 && sec===0)
+    return  mas[thir]+" "+m[8];
+
+    if(fir!==0 && sec===0)
+    return  mas[thir]+" "+m[8]+" "+mas[fir];
+
+    if(fir!==0 && sec===1)
+    return  mas[thir]+" "+m[8]+" "+mas[fir+10];
+    if(fir===0 && sec===1)
+    return  mas[thir]+" "+m[8]+" "+mas[10];
+
+    if(fir===0 && sec!==0)
+    return  mas[thir]+" "+m[8]+ " " + m[sec-2];   
+    if(fir!==0 && sec!==0)
+    return  mas[thir]+" "+m[8]+ " " + m[sec-2] +" "+mas[fir];     
+
+
+
+
+return ;
 }
